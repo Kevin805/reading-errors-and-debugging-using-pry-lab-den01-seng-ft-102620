009000d37e42a038e7f2fb3b7d0d814645c9911b
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-12 19:34:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,8 +2,8 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-def state_log(star date)
-  "Captain's Log, star date #{star date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 def engage
   puts state_log(date)
